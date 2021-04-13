ebaf94bacc10c1963791981017e5ce67d04ad3b3
Bobrossrtx <bobrossrtx@gmail.com>
2021-04-13 17:25:11 +0100

    Added TCP.server.ts

--- Learning/First Steps/TCP.server.ts
+++ Learning/First Steps/TCP.server.ts
@@ -2,6 +2,6 @@ const hostname:string = "0.0.0.0";
 const port:number = 8080;
 const listener:Deno.Listener = Deno.listen({ hostname, port });
 console.log(`Listening on ${hostname}:${port}`);
-for await (const conn of listener) { 
+for await (const conn of listener) {
     Deno.copy(conn, conn);
 }
